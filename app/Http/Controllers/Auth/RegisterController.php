787f8805641fb169d657ec1f6a2fdbba07659448
sw33tsoda <sw33tsoda@gmail.com>
2021-03-13 12:47:01 +0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'balance' => 200000000
         ]);
     }
 }
