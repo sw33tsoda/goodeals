1d8671cdfa6fb0493a4a8658cf582d0d799be46a
sw33tsoda <sw33tsoda@gmail.com>
2021-03-13 14:44:50 +0700

    update

--- config/database.php
+++ config/database.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Str;
 
+$host = $port = $username = $password = $database = '';
+if(getenv("CLEARDB_DATABASE_URL")){
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $post = $url["port"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
+
 return [
 
     /*
